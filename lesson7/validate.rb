893e5adf2ab5cbe259d9a181d4aeb55fc7afc9a8
ChuDik <chudik@inbox.ru>
2020-04-02 10:08:11 +0500

    remove def validate! to classes

--- lesson7/validate.rb
+++ lesson7/validate.rb
@@ -2,15 +2,7 @@ module Validate
   def valid?
     validate!
     true
-    puts "validate true"
   rescue
     false
-    puts "validate false"
   end
-
-  protected
-  def validate!
-    raise "Название не может быть пустым!" if @title == ""
-  end
-
 end
