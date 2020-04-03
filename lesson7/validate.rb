f6a92bb795e878d0959947e0ec918d019f585918
ChuDik <chudik@inbox.ru>
2020-04-03 10:35:08 +0500

    add validate_*! defs for all classes

--- lesson7/validate.rb
+++ lesson7/validate.rb
@@ -5,4 +5,21 @@ module Validate
   rescue
     false
   end
+
+  def validate_number!
+    raise "Номер не должен быть пустым!" if number.empty?
+  end
+
+  def validate_type!
+    raise "Тип не должен быть пустым!" if type.empty?
+  end
+
+  def validate_title!
+    raise "Название не должно быть пустым!" if title.empty?
+  end
+
+  def validate_route!
+    raise "Обе станции не должны быть пустыми!" if start.empty? || finish.empty?
+  end
+
 end
