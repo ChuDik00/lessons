1c7126162c50bd412ac651bc4f022e4cd2b4759f
ChuDik <chudik@inbox.ru>
2020-04-13 11:12:07 +0500

    add presence, format, type methods

--- lesson10/validation.rb
+++ lesson10/validation.rb
@@ -14,6 +14,29 @@ module Validation
   end
 
   module InstanceMethods
+    def validate!
 
+    end
+
+    def valid?
+      validate!
+      true
+    rescue
+      false
+    end
+
+    protected
+
+    def presence(name, val, _args)
+      raise "#{name} is not present" if val.nil? || val == ''
+    end
+
+    def format(name, val, format)
+      raise 'Wrong format!' if val !~ format
+    end
+
+    def type(name, val, type)
+      raise 'Class mismatch!' if val.class != type
+    end
   end
 end
