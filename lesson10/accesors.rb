421cf273bab0e22c5310e40e9155923412f1646e
ChuDik <chudik@inbox.ru>
2020-04-13 09:50:51 +0500

    did attr_accessor_with_history method, add strong_attr_accessor method

--- lesson10/accesors.rb
+++ lesson10/accesors.rb
@@ -1,5 +1,19 @@
-module Accesors
-  def attr_accesor_with_history
+module Accessors
+  def attr_accesor_with_history(*attrs)
+    attrs.each do |attr|
+      attr_name = "@#{attr}".to_sym
+      define_method(attr) { instance_variable_get(attr_name) }
+      define_method("#{attr}=".to_sym) do |val|
+        instance_variable_set(attr_name, val)
+        @history_val ||= {}
+        @history_val[attr] ||= []
+        @history_val[attr] << val
+      end
+      define_method("#{attr}_history") { @history_val ? @history_val[attr] : [] }
+    end
+  end
+
+  def strong_attr_accessor
 
   end
 end
